9588590f4dc26d810dbb1a158b945e8d82c6d4de
Manuel González <mgmerino@gmail.com>
2025-12-03 00:18:41 +0100

    Add public API

--- lib/reseg.rb
+++ lib/reseg.rb
@@ -18,7 +18,39 @@ require_relative "reseg/parsing/segment_parser"
 require_relative "reseg/parsing/statement"
 require_relative "reseg/parsing/scanner"
 
+# Public API
 module Reseg
   class Error < StandardError; end
-  # Your code goes here...
+
+  Result = Struct.new(:trips, :errors) do
+    def success?
+      errors.empty?
+    end
+  end
+
+  def self.format(input, based_city:, time_zone: nil)
+    result = parse(input, based_city:, time_zone:)
+
+    return result.errors.join("\n") unless result.success?
+
+    result.trips.map(&:to_s).join("\n")
+  end
+
+  def self.parse(input, based_city:, time_zone: nil)
+    context = Context.new(based_city:, time_zone:)
+
+    statements = []
+    Parsing::Scanner.new(input).each { |st| statements << st }
+
+    reservation_builder = ReservationBuilder.new(statements:, context:)
+    reservation_builder.build
+
+    segments = reservation_builder.segments
+    trip_builder = TripBuilder.new(segments:, context:)
+    trip_builder.build
+
+    errors = reservation_builder.errors + trip_builder.errors
+
+    Result.new(trip_builder.trips, errors)
+  end
 end
